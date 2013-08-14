6efd20a90b8cc90bd696ade97522b35ae2222ad8
chavp <my.parinya@gmail.com>
2013-08-14 23:50:01 +0700

    add Auction Domain Model
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ AopInDotNet/Auction/Member.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AopInDotNet.Auction
+{
+    public class Member
+    {
+        public string LoginName { get; set; }
+        public string ReputationPoints { get; set; }
+    }
+}
