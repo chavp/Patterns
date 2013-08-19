540e8e9a374815a67cabe5ced551554983f569a1
chavp <my.parinya@gmail.com>
2013-08-20 00:12:12 +0700

    add LoggerInstaller example
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ AopInDotNet/CastleWindsor/King.cs
@@ -0,0 +1,21 @@
+﻿using Castle.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AopInDotNet.CastleWindsor
+{
+    public class King
+        : IKing
+    {
+        public ILogger Logger { get; set; }
+
+        public void RuleTheCastle()
+        {
+            Logger.Info("Begin RuleTheCastle");
+            
+        }
+    }
+}
