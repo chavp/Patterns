469297d3ff9847156524de83c542e0bad85079f9
chavp <my.parinya@gmail.com>
2013-08-27 06:02:50 +0700

    ChavpWeb
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ ChavpWeb/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ChavpWeb.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static MvcHtmlString KoTextBoxFor<TModel, TProp>(
+            this HtmlHelper<TModel> htmlHelper, 
+            Expression<Func<TModel, TProp>> expression,
+            object htmlAttributes)
+        {
+
+            return global::System.Web.Mvc.Html.InputExtensions.TextBoxFor<TModel, TProp>(
+                htmlHelper, expression, htmlAttributes);
+        }
+    }
+}
