ec3b627dc3ba9620197ac6375e301bb2c010be3f
chavp <my.parinya@gmail.com>
2013-09-12 17:27:18 +0700

    add QUnit JavaScript Design Pattern
    Signed-off-by: chavp <my.parinya@gmail.com>

---
+++ ChavpWeb/Scripts/patterns/Car.js
@@ -0,0 +1,13 @@
+﻿// A car "class"
+function Car(model) {
+    var self = this;
+
+    self.model = model;
+    self.color = "silver";
+    self.year = "2012";
+
+    self.getInfo = function () {
+        return self.model + " " + self.year;
+    };
+
+}
